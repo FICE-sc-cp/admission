3d3d1cbbae3e0ddf31de95cf5c90194a2367bee5
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-07-03 19:50:25 +0300

    feat: added database with prisma & config

---
+++ admission-api/src/config/config.ts
@@ -0,0 +1,7 @@
+import * as process from 'process';
+
+export default () => ({
+  port: parseInt(process.env.PORT) || 3000,
+  databaseUrl: process.env.DATABASE_URL,
+  admissionEmail: process.env.ADMISSION_EMAIL,
+});

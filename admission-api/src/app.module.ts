977d359317f64fe79f3c9ba8439db7a3190a5849
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-07-03 19:50:25 +0300

    feat: added database with prisma & config

--- admission-api/src/app.module.ts
+++ admission-api/src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+  ],
 })
 export class AppModule {}

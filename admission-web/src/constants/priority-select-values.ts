4d0ba08e9de1097e7517d76f10150667b9179a35
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2024-07-17 13:44:25 +0300

    added page for all priorities

--- admission-web/src/constants/priority-select-values.ts
+++ admission-web/src/constants/priority-select-values.ts
@@ -1,5 +1,26 @@
-export const prioritySelectValues = [
-  'Інтегровані інформаційні системи',
-  'Інформаційні управляючі системи та технології',
-  'Інформаційне забезпечення робототехнічних систем',
+import { EducationProgram } from '@admission/utils';
+
+export const IPeduPrograms = [
+  {
+    label: "Інженерія програмного забезпечення комп'ютерних систем",
+    id: EducationProgram.CSSE,
+  },
+  {
+    label: 'Інженерія програмного забезпечення інформаційних систем',
+    id: EducationProgram.ISSE,
+  },
+];
+export const ISTeduPrograms = [
+  {
+    label: 'Інформаційне забезпечення робототехнічних систем',
+    id: EducationProgram.ISRS,
+  },
+  {
+    label: 'Інтегровані інформаційні системи',
+    id: EducationProgram.IIS,
+  },
+  {
+    label: 'Інформаційні управляючі системи та технології',
+    id: EducationProgram.IMST,
+  },
 ];

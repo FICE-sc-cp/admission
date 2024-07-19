d230ba77d86f688e43f0346dc44a668e1c4aeec5
kujo205 <kuc8301@gmail.com>
2024-07-20 01:17:20 +0300

    fix(web): add validation schema to forms

--- admission-web/src/schemas-and-types/priority.ts
+++ admission-web/src/schemas-and-types/priority.ts
@@ -1,20 +1,36 @@
 import { z } from 'zod';
 import { EducationProgram } from '$/utils/src';
 
-export const priorityFormSchema = z.object({
-  priority_1: z.string({
-    required_error: 'Будь ласка оберіть приорітет',
-  }),
-  priority_2: z.string({
-    required_error: 'Будь ласка оберіть приорітет',
-  }),
-  priority_3: z
-    .string({
-      required_error: 'Будь ласка оберіть приорітет',
-    })
-    .optional(),
-  date: z.string().readonly(),
-});
+export const priorityFormSchema = z
+  .object({
+    priority_1: z.string({
+      required_error: 'Будь ласка оберіть пріорітет',
+    }),
+    priority_2: z.string({
+      required_error: 'Будь ласка оберіть пріорітет',
+    }),
+    priority_3: z
+      .string({
+        required_error: 'Будь ласка оберіть пріорітет',
+      })
+      .optional(),
+    date: z.string().readonly(),
+  })
+  .refine(({ priority_1, priority_2, priority_3 }) => {
+    const priorities = [priority_1, priority_2, priority_3];
+
+    for (let i = 0; i < priorities.length; i++) {
+      for (let j = 0; j < priorities.length; j++) {
+        if (i === j) continue;
+
+        if (priorities[i] === priorities[j]) {
+          console.log('match');
+          return false;
+        }
+      }
+    }
+    return true;
+  }, 'Пріорітети повинні бути унікальними');
 
 export const TPriorityForm = typeof priorityFormSchema;
 

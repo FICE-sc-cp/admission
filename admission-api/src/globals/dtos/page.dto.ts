c80c6d4a0ff8ead4e6e98743b26e134a3d57754f
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-07-26 10:47:28 +0300

    fixed pagination

--- admission-api/src/globals/dtos/page.dto.ts
+++ admission-api/src/globals/dtos/page.dto.ts
@@ -1,9 +1,12 @@
 import { Optional } from '../decorators';
+import { Transform } from 'class-transformer';
 
 export class PageDto {
   @Optional()
+  @Transform(({ value }) => +value)
     take: number;
 
   @Optional()
+  @Transform(({ value }) => +value)
     skip: number;
 }

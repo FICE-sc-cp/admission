bdeddca88626369b391e9c206cd1f70b3f4e842f
VlO-OlV <linichenko.vanya@gmail.com>
2024-07-26 22:40:52 +0300

    feature: added endpoint to download docs

--- admission-api/src/api/documents/document.controller.ts
+++ admission-api/src/api/documents/document.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Res, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { DocumentService } from './document.service';
@@ -6,6 +6,7 @@ import { ContractDto } from './dto/contract.dto';
 import { ContractByIdPipe } from './pipes/contract-by-id.pipe';
 import { UpdateContractDto } from './dto/update-contract.dto';
 import { CreateContractDto } from './dto/create-contract.dto';
+import { Response } from 'express';
 
 @ApiTags('Documents')
 @Controller({
@@ -24,6 +25,48 @@ export class DocumentController {
     @Body() body: CreateContractDto,
   ): Promise<ContractDto> {
     return this.documentService.createDocuments(body);
+  }
+
+  @Get('/:contractId/download/contract')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Download user contract' })
+  @ApiResponse({ type: Buffer })
+  async downloadContract (
+    @Param('contractId', ContractByIdPipe) contractId: string,
+    @Res() response: Response,
+  ): Promise<void> {
+    const file = await this.documentService.downloadContract(contractId);
+    response.header('Content-Type', file.contentType);
+    response.header('Content-Disposition', `attachment; filename=${file.fileName}`);
+    response.send(file.data);
+  }
+
+  @Get('/:contractId/download/payment')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Download user payment' })
+  @ApiResponse({ type: Buffer })
+  async downloadPayment (
+    @Param('contractId', ContractByIdPipe) contractId: string,
+    @Res() response: Response,
+  ): Promise<void> {
+    const file = await this.documentService.downloadPayment(contractId);
+    response.header('Content-Type', file.contentType);
+    response.header('Content-Disposition', `attachment; filename=${file.fileName}`);
+    response.send(file.data);
+  }
+
+  @Get('/:contractId/download/priority')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Download user priority' })
+  @ApiResponse({ type: Buffer })
+  async downloadPriority (
+    @Param('contractId', ContractByIdPipe) contractId: string,
+    @Res() response: Response,
+  ): Promise<void> {
+    const file = await this.documentService.downloadPriority(contractId);
+    response.header('Content-Type', file.contentType);
+    response.header('Content-Disposition', `attachment; filename=${file.fileName}`);
+    response.send(file.data);
   }
 
   @Patch('/:contractId')

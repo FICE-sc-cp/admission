8177778f0a011eac4cc2e6a7dc7bbfe6ca3dd0b3
sifes <lonfark@gmail.com>
2024-07-27 10:08:52 +0300

    add toaster component

--- admission-web/src/app/layout.tsx
+++ admission-web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '@/hooks/auth-provider';
+import { Toaster } from '@/components/ui/toast/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang='uk'>
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          {children}
+          <Toaster />
+        </AuthProvider>
       </body>
     </html>
   );

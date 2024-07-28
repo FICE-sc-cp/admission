fcbff069ec93f98678cc495ce109798e93157a07
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2024-07-29 01:01:48 +0300

    feature/personal-data-page (#42)
    * created progress stepper
    
    * finished progress stepper
    
    * added entrant form
    
    * added other forms
    
    * added representatitve type
    
    * updated dependecies
    
    * finished page
    
    * added responsive design
    
    * Update SubmitPage.tsx
    
    * fixes
    
    * fixed build errors
    
    * add client rendering
    
    * fixes
    
    * fixed build
    
    * Update page.tsx
    
    ---------
    
    Co-authored-by: sifes <lonfark@gmail.com>

---
+++ admission-web/src/app/(application)/personal-data/_components/progress-stepper/_components/Step.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import { Separator } from '@/components/ui/separator';
+import { Check } from 'lucide-react';
+
+interface StepProps {
+  activeStep: number;
+  isCompleted: boolean;
+  stepNumber: number;
+  text: string;
+  isLast: boolean;
+}
+
+const Step: FC<StepProps> = ({
+  activeStep,
+  isCompleted,
+  stepNumber,
+  text,
+  isLast,
+}) => {
+  return (
+    <div className='flex flex-col items-center gap-3'>
+      <div
+        className={`relative w-fit rounded-[50%] border-2 ${activeStep === stepNumber || isCompleted ? 'border-violet-600' : 'border-slate-300'} ${isCompleted ? 'bg-violet-600' : 'bg-white'} p-1.5`}
+      >
+        <div
+          className={`flex w-[30px] flex-col items-center text-center text-xl ${activeStep === stepNumber ? 'text-violet-600' : 'text-slate-700'} `}
+        >
+          {isCompleted ? <Check className='h-7 w-8 text-white' /> : stepNumber}
+        </div>
+        {!isLast && (
+          <Separator
+            orientation='horizontal'
+            className={`${isCompleted ? 'bg-violet-500' : 'bg-slate-300'} absolute left-[40px] top-[20px] z-[-1] h-[2px] w-[62px] md:w-[195px]`}
+          />
+        )}
+      </div>
+      <p className='hidden text-sm md:block'>{text}</p>
+    </div>
+  );
+};
+
+export default Step;

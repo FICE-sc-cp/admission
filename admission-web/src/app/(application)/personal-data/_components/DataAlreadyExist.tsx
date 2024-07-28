d0eb6d31086ad386ca74d4492d2bfd28da9d637d
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2024-07-29 01:01:48 +0300

    feature/personal-data-page (#42)
    * created progress stepper
    
    * finished progress stepper
    
    * added entrant form
    
    * added other forms
    
    * added representatitve type
    
    * updated dependecies
    
    * finished page
    
    * added responsive design
    
    * Update SubmitPage.tsx
    
    * fixes
    
    * fixed build errors
    
    * add client rendering
    
    * fixes
    
    * fixed build
    
    * Update page.tsx
    
    ---------
    
    Co-Authored-By: sifes <lonfark@gmail.com>

---
+++ admission-web/src/app/(application)/personal-data/_components/DataAlreadyExist.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+const DataAlreadyExist = () => {
+  const { push } = useRouter();
+  return (
+    <div className='m-auto flex flex-col items-center gap-[16px]'>
+      <img
+        src='/giphy-frog.gif'
+        alt='frogs'
+        className='w-[286px] md:w-[382px]'
+      />
+      <div className='flex flex-col items-center gap-[12px]'>
+        <p className='text-center text-xl'>Особисту інформацію вже заповнено</p>
+        <p className='text-md text-center'>
+          Заповнену інформацію ви можете переглянути в профілі свого акаунту{' '}
+        </p>
+      </div>
+      <Button className='w-fit' onClick={() => push('/')}>
+        До профілю
+      </Button>
+    </div>
+  );
+};
+
+export default DataAlreadyExist;
---
+++ admission-web/src/lib/schemas-and-types/personal-data/personal-data.ts
@@ -0,0 +1,153 @@
+import { z } from 'zod';
+import {
+  dateRegex,
+  kirillicRegex,
+  ukNumberRegex,
+  ukRegex,
+} from '@/lib/constants/regex';
+
+export const EntrantSchema = z
+  .object({
+    phoneNumber: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(ukNumberRegex, 'Номер має містити 9 цифр'),
+    passportNumber: z.string({ required_error: "Обов'язкове поле" }),
+    email: z
+      .string({ required_error: `Обов'язкове поле` })
+      .email('Введіть емейл'),
+    passportDate: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(dateRegex, 'Має бути формату dd.mm.yyyy'),
+    passportInstitute: z
+      .string({ required_error: "Обов'язкове поле" })
+      .min(4, 'Орган видачі має містити 4 цифри'),
+    passportSeries: z
+      .string()
+      .max(2, 'Серія паспорту має містити 2 символи')
+      .regex(
+        kirillicRegex,
+        'Серія паспорту має містити кириличні літери верхнього регістру'
+      )
+      .nullable()
+      .default('ТТ'),
+    idCode: z
+      .string({ required_error: "Обов'язкове поле" })
+      .max(10, 'ІПН має містити 10 цифр')
+      .nullable(),
+    region: z.string({ required_error: 'Будь ласка оберіть регіон' }),
+    settlement: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс')
+      .nullable(),
+    address: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс'),
+    index: z
+      .string({ required_error: "Обов'язкове поле" })
+      .max(5, 'Має містити 5 цифр'),
+    study_form: z.enum(['Бюджет', 'Контракт']).default('Контракт'),
+    submission_in_corpus: z.boolean().default(false).optional(),
+    oldPassportTemplate: z.boolean().default(false).optional(),
+  })
+  .superRefine((data, ctx) => {
+    const passportNumber = data.passportNumber;
+    const oldPassportTemplate = data.oldPassportTemplate;
+    const length = passportNumber.length;
+
+    if (oldPassportTemplate && length !== 6) {
+      return ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Номер паспорту має містити 6 цифр',
+        path: ['passportNumber'],
+      });
+    } else if (!oldPassportTemplate && length !== 9) {
+      return ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Номер паспорту має містити 9 цифр',
+        path: ['passportNumber'],
+      });
+    }
+
+    return ctx;
+  });
+
+export const PersonalDataSchema = z
+  .object({
+    lastName: z
+      .string({ required_error: `Обов'язкове поле` })
+      .min(2, 'Не коротше 2 символів')
+      .max(40, 'Не довше 40 символів')
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс'),
+    firstName: z
+      .string({ required_error: `Обов'язкове поле` })
+      .min(2, 'Не коротше 2 символів')
+      .max(40, 'Не довше 40 символів')
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс'),
+    middleName: z
+      .string({ required_error: `Обов'язкове поле` })
+      .min(2, 'Не коротше 2 символів')
+      .max(40, 'Не довше 40 символів')
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс')
+      .nullable(),
+    email: z
+      .string({ required_error: `Обов'язкове поле` })
+      .email('Введіть емейл'),
+    phoneNumber: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(ukNumberRegex, 'Номер має містити 9 цифр'),
+    passportNumber: z.string({ required_error: "Обов'язкове поле" }),
+    passportDate: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(dateRegex, 'Має бути формату dd.mm.yyyy'),
+    passportInstitute: z
+      .string({ required_error: "Обов'язкове поле" })
+      .min(4, 'Орган видачі має містити 4 цифри'),
+    passportSeries: z
+      .string({ required_error: "Обов'язкове поле" })
+      .max(2, 'Серія паспорту має містити 2 символи')
+      .regex(
+        kirillicRegex,
+        'Серія паспорту має містити кириличні літери верхнього регістру'
+      )
+      .nullable()
+      .default('ТТ'),
+    idCode: z
+      .string({ required_error: "Обов'язкове поле" })
+      .max(10, 'ІПН має містити 10 цифр')
+      .nullable(),
+    region: z.string({ required_error: 'Будь ласка оберіть регіон' }),
+    settlement: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс'),
+    address: z
+      .string({ required_error: "Обов'язкове поле" })
+      .regex(ukRegex, 'Має містити українські літери, апостроф або дефіс'),
+    index: z
+      .string({ required_error: "Обов'язкове поле" })
+      .max(5, 'Має містити 5 цифр'),
+    oldPassportTemplate: z.boolean().default(false).optional(),
+  })
+  .superRefine((data, ctx) => {
+    const passportNumber = data.passportNumber;
+    const oldPassportTemplate = data.oldPassportTemplate;
+    const length = passportNumber.length;
+
+    if (oldPassportTemplate && length !== 6) {
+      return ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Номер паспорту має містити 6 цифр',
+        path: ['passportNumber'],
+      });
+    } else if (!oldPassportTemplate && length !== 9) {
+      return ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Номер паспорту має містити 9 цифр',
+        path: ['passportNumber'],
+      });
+    }
+
+    return ctx;
+  });
+
+export type TEntrantSchema = z.infer<typeof EntrantSchema>;
+export type TPersonalDataSchema = z.infer<typeof PersonalDataSchema>;

1b7549db1fdc34e34e8004028140745ebd92c393
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-07-29 19:09:01 +0300

    removed role check

--- admission-api/src/api/users/pipes/user-by-id.pipe.ts
+++ admission-api/src/api/users/pipes/user-by-id.pipe.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import { Role } from '@prisma/client';
 import { UserRepo } from 'src/database/repo/user.repo';
-import { ENTRANT_WITH_SUCH_ID_DOES_NOT_EXIST_MSG, USER_WITH_SUCH_ID_DOES_NOT_EXIST_MSG } from '../constants';
+import { USER_WITH_SUCH_ID_DOES_NOT_EXIST_MSG } from '../constants';
 
 @Injectable()
 export class UserByIdPipe implements PipeTransform<string, Promise<string>> {
@@ -13,9 +12,6 @@ export class UserByIdPipe implements PipeTransform<string, Promise<string>> {
     const user = await this.userRepo.find({ id: entrantId });
     if (!user) {
       throw new BadRequestException(USER_WITH_SUCH_ID_DOES_NOT_EXIST_MSG);
-    }
-    if (user.role !== Role.ENTRANT) {
-      throw new BadRequestException(ENTRANT_WITH_SUCH_ID_DOES_NOT_EXIST_MSG);
     }
     return entrantId;
   }

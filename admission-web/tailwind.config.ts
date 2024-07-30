9cc0f17eaabfa9ef5bad269e6469f0518e156265
sifes <lonfark@gmail.com>
2024-07-30 14:19:31 +0300

    refactor navigation menu and main content wrapper

--- admission-web/tailwind.config.ts
+++ admission-web/tailwind.config.ts
@@ -17,13 +17,6 @@ const config = {
     '2xl': '1536px',
   },
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
       backgroundImage: {
         'signin-bg': 'url(/signin-bg.jpg)',

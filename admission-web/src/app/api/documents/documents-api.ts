41ed1ecef16c6253c2922a0c736a40717676e497
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2024-07-31 15:54:29 +0300

    modified documents api

--- admission-web/src/app/api/documents/documents-api.ts
+++ admission-web/src/app/api/documents/documents-api.ts
@@ -5,6 +5,32 @@ class DocumentsApi {
   async createDocument(body: DocumentsApiBody) {
     await instance.post('/documents', body);
   }
+
+  async updateDocument(body: DocumentsApiBody, contractId: string) {
+    await instance.patch(`/documents/${contractId}`, body);
+  }
+
+  async deleteDocument(contractId: string) {
+    await instance.delete(`documents/${contractId}`);
+  }
+
+  async downloadContract(contractId: string) {
+    return instance.get(`/documents/${contractId}/download/contract`, {
+      responseType: 'blob',
+    });
+  }
+
+  async downloadPayment(contractId: string) {
+    return instance.get(`/documents/${contractId}/download/payment`, {
+      responseType: 'blob',
+    });
+  }
+
+  async downloadPriority(contractId: string) {
+    return instance.get(`/documents/${contractId}/download/priority`, {
+      responseType: 'blob',
+    });
+  }
 }
 
 export default new DocumentsApi();

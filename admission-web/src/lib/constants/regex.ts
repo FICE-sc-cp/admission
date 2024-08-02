c0c8f7c203c2e43ff6315185fd55d196eed630a4
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-08-03 02:25:26 +0300

    added / into validation

--- admission-web/src/lib/constants/regex.ts
+++ admission-web/src/lib/constants/regex.ts
@@ -1,5 +1,5 @@
 export const kirillicRegex = /^[А-Я]{2}$/;
-export const ukRegex = /^[ҐЄІЇЬА-ЩЮЯґєіїьа-щюя0-9\-`'’‘“”*,. ]+$/;
+export const ukRegex = /^[ҐЄІЇЬА-ЩЮЯґєіїьа-щюя0-9\-`'’‘“”*,. /]+$/;
 export const dateRegex =
   /^\s*((?:3[01]|[12][0-9]|0[1-9])\.(1[012]|0[1-9])\.((?:19|20)\d{2}))\s*$/;
 

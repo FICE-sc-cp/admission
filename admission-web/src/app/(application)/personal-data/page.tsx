ad834940c55f73860c26e198b0d8560cb8346e96
sifes <lonfark@gmail.com>
2024-08-02 16:30:28 +0300

    add checking whether user has filled personal data

--- admission-web/src/app/(application)/personal-data/page.tsx
+++ admission-web/src/app/(application)/personal-data/page.tsx
@@ -1,17 +1,42 @@
 'use client';
 
+import { Loader } from '@/components/common/components/Loader';
 import CustomerPage from '@/components/pages/entrant/personal-data/components/CustomerPage';
 import EntrantForm from '@/components/pages/entrant/personal-data/components/EntrantForm';
 import ProgressStepper from '@/components/pages/entrant/personal-data/components/ProgressStepper';
 import RepresentativePage from '@/components/pages/entrant/personal-data/components/RepresentativePage';
 import SubmitPage from '@/components/pages/entrant/personal-data/components/SubmitPage';
 import { usePersonalDataContext } from '@/lib/contexts/PersonalDataContext';
+import useAuth from '@/lib/hooks/useAuth';
 import { useState, useEffect } from 'react';
+import PersonalDataApi from '@/app/api/personal-data/personal-data';
+import { useCommonToast } from '@/components/ui/toast/use-common-toast';
+import DataAlreadyExist from '@/components/pages/entrant/personal-data/components/DataAlreadyExist';
 
 const PersonalDataPage = () => {
   const { isAdult, isAnotherPayer, activeStep } = usePersonalDataContext();
-
   const [steps, setSteps] = useState<string[]>([]);
+  const { toastError } = useCommonToast();
+  const [loading, setLoading] = useState(true);
+  const [isAlreadyFilled, setIsAlreadyFilled] = useState(true);
+
+  const { user, loading: userLoading } = useAuth();
+
+  const getUserPersonalData = async (userId: string) => {
+    try {
+      const { data } = await PersonalDataApi.getPersonalData(userId);
+      setIsAlreadyFilled(data.entrantData !== null);
+    } catch (error) {
+      toastError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (user) {
+      getUserPersonalData(user.id);
+    }
+  }, [user]);
 
   useEffect(() => {
     const stepsArr = ['Інформація про вступника', 'Підтвердження даних'];
@@ -28,15 +53,27 @@ const PersonalDataPage = () => {
     setSteps(stepsArr);
   }, [isAdult, isAnotherPayer]);
 
+  if (loading || userLoading) {
+    return <Loader />;
+  }
+
   return (
     <main className='flex flex-1 flex-col items-center gap-4 p-4 lg:gap-6 lg:p-6'>
-      <ProgressStepper activeStep={activeStep} steps={steps} />
-      {activeStep === 1 && <EntrantForm />}
-      {steps[activeStep - 1] === 'Законний представник' && (
-        <RepresentativePage />
+      {isAlreadyFilled ? (
+        <DataAlreadyExist />
+      ) : (
+        <>
+          <ProgressStepper activeStep={activeStep} steps={steps} />
+          {activeStep === 1 && <EntrantForm />}
+          {steps[activeStep - 1] === 'Законний представник' && (
+            <RepresentativePage />
+          )}
+          {steps[activeStep - 1] === 'Інформація про платника' && (
+            <CustomerPage />
+          )}
+          {activeStep === steps.length && <SubmitPage />}
+        </>
       )}
-      {steps[activeStep - 1] === 'Інформація про платника' && <CustomerPage />}
-      {activeStep === steps.length && <SubmitPage />}
     </main>
   );
 };
--- admission-web/src/components/pages/entrant/personal-data/components/DataAlreadyExist.tsx
+++ admission-web/src/components/pages/entrant/personal-data/components/DataAlreadyExist.tsx
@@ -1,26 +1,22 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const DataAlreadyExist = () => {
-  const { push } = useRouter();
   return (
-    <div className='m-auto flex flex-col items-center gap-[16px]'>
-      <img
-        src='/giphy-frog.gif'
-        alt='frogs'
-        className='w-[286px] md:w-[382px]'
-      />
-      <div className='flex flex-col items-center gap-[12px]'>
-        <p className='text-center text-xl'>Особисту інформацію вже заповнено</p>
+    <div className='m-auto mt-[20%] flex flex-col items-center gap-4'>
+      <Image src='/giphy-frog.gif' alt='frogs' width={286} height={160} />
+      <div className='flex flex-col items-center gap-3'>
+        <h3 className='text-center text-xl font-semibold'>
+          Особисту інформацію вже заповнено
+        </h3>
         <p className='text-md text-center'>
-          Заповнену інформацію ви можете переглянути в профілі свого акаунту{' '}
+          Заповнену інформацію ви можете переглянути в профілі свого акаунту
         </p>
       </div>
-      <Button className='w-fit' onClick={() => push('/')}>
-        До профілю
-      </Button>
+      <Link href='/'>
+        <Button className='w-fit'>До профілю</Button>
+      </Link>
     </div>
   );
 };

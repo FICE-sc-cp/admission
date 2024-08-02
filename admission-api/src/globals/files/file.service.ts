42574554e2d95f6855268fb6f10bcb372e13865b
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-08-03 01:35:40 +0300

    changed path to files 2

--- admission-api/src/globals/files/file.service.ts
+++ admission-api/src/globals/files/file.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import Docxtemplater from 'docxtemplater';
 import * as fs from 'fs';
-import { join } from 'path';
+import { join, resolve as resolvePath } from 'path';
 import PizZip from 'pizzip';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FileService {
+  constructor (
+    private readonly configService: ConfigService,
+  ) {}
+
   fillTemplate (fileName: string, data: object) {
-    const path = join(__dirname, '..', 'private/templates', fileName);
+    const nodeEnv = this.configService.get<string>('nodeEnv');
+    const path = join(nodeEnv === 'local' ? resolvePath() : '/var/task/admission-api', 'private/templates', fileName);
     const zip = new PizZip(fs.readFileSync(path, 'binary'));
   
     const doc = new Docxtemplater(zip, {

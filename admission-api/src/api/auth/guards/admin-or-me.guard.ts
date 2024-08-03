ee89b0cd2e98f43a1dddb108b51c905df4d4fd19
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2024-08-04 00:29:24 +0300

    removed local

--- admission-api/src/api/auth/guards/admin-or-me.guard.ts
+++ admission-api/src/api/auth/guards/admin-or-me.guard.ts
@@ -1,20 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { UserDto } from '../../users/dtos/user.dto';
 import { Request } from 'express';
 import { Role } from '@prisma/client';
 
 @Injectable()
 export class AdminOrMeGuard implements CanActivate {
-  constructor (
-    private readonly configService: ConfigService,
-  ) {}
+  constructor () {}
 
   async canActivate (context: ExecutionContext): Promise<boolean> {
-    const nodeEnv = this.configService.get<string>('nodeEnv');
-
-    if (nodeEnv === 'local') return true;
-
     const request = context.switchToHttp().getRequest<Request & { user: UserDto }>();
     const user = request.user;
 

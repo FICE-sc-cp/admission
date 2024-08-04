9fb706e7a6bd59f24ee782a02ca06d4968b1a173
sifes <lonfark@gmail.com>
2024-08-04 19:59:27 +0300

    add suppressHydrationWarning

--- admission-web/src/app/layout.tsx
+++ admission-web/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='uk'>
+    <html lang='uk' suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
   );
